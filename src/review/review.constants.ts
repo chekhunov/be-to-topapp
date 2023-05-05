39f9384b7d2afe33c945057cbca23675d71668ce
Ihor <scdesignk@gmail.com>
2023-05-05 20:50:11 +0200

    [Feature] Test review routes

---
+++ src/review/review.constants.ts
@@ -0,0 +1,1 @@
+export const REVIEW_NOT_FOUND = 'this is review not found';
---
+++ src/review/review.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { ReviewModel } from './review.model';
+import { DocumentType, ReturnModelType } from '@typegoose/typegoose';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { Types } from 'mongoose';
+import { InjectModel } from 'nestjs-typegoose';
+import { ModelType } from '@typegoose/typegoose/lib/types';
+
+@Injectable()
+export class ReviewService {
+  constructor(
+    @InjectModel(ReviewModel)
+    private readonly reviewModel: ReturnModelType<typeof ReviewModel>,
+  ) {}
+
+  async create(dto: CreateReviewDto): Promise<DocumentType<ReviewModel>> {
+    return this.reviewModel.create(dto);
+  }
+
+  async delete(id: string): Promise<DocumentType<ReviewModel> | null> {
+    return this.reviewModel.findByIdAndDelete(id).exec();
+  }
+
+  async findByProductId(
+    productId: string,
+  ): Promise<DocumentType<ReviewModel>[]> {
+    return this.reviewModel
+      .find({ productId: Types.ObjectId(productId) })
+      .exec();
+  }
+
+  async deleteByProductId(
+    productId: string,
+  ): Promise<DocumentType<ReviewModel> | null> {
+    return this.reviewModel
+      .deleteMany({ productId: Types.ObjectId(productId) })
+      .exec();
+  }
+}
